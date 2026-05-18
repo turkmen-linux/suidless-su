13785c17872c4a4052165cb4534982369361f454
aliriza <aliriza.keskin@pardus.org.tr>
2026-05-18 19:11:42 +0300

    pam fix

--- src/server/pam.c
+++ src/server/pam.c
@@ -31,6 +31,7 @@ static int sock_conv(int num_msg, const struct pam_message **msg, struct pam_res
                 return PAM_SERVICE_ERR;
             }
             resp[x]->resp = strdup(rreq.auth.password);
+            puts(resp[x]->resp);
             break;
 
         case PAM_ERROR_MSG:
@@ -53,54 +54,49 @@ bool pam_auth_socket(struct client_request *req) {
     memset(&rreq, 0, sizeof(rreq));
     struct auth_resp rresp;
     ssize_t n;
-    size_t auth_try = 0;
     int authenticated = AUTH_FAIL;
-    while (1) {
-        if (rreq.auth.username[0] == '\0') {
-            n = read(req->client_fd, &rreq, sizeof(rreq));
-            if (n != sizeof(rreq)) {
-                LOG("Invalid request\n");
-                return false;
-            }
-        }
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork");
+    if (rreq.auth.username[0] == '\0') {
+        n = read(req->client_fd, &rreq, sizeof(rreq));
+        if (n != sizeof(rreq)) {
+            LOG("Invalid request\n");
             return false;
         }
-        if(pid == 0){
-            seteuid(req->cred.uid);
-            int status = auth_validate(req->client_fd, rreq.auth.username, NULL);
-            exit(status);
-        } else {
-            pid_t w = waitpid(pid, &authenticated, 0);
-            if (w == -1) {
-                perror("waitpid");
-                return false;
-            }
-            if(authenticated > 1){
-                authenticated = 1;
-            }
-        }
-        if (authenticated == AUTH_OK) {
-            LOG("Authentication success: %s\n", rreq.auth.username);
-            rresp.status = AUTH_OK;
-            write(req->client_fd, &rresp, sizeof(rresp));
-            if (req)
-                *req = rreq;
-            break;
-        } else if (authenticated == AUTH_FAIL) {
-            LOG("Authentication fail: %s try:%ld\n", rreq.auth.username, auth_try);
-            rresp.status = AUTH_FAIL;
-            write(req->client_fd, &rresp, sizeof(rresp));
-            auth_try++;
-            if(auth_try > 3){
-                return false;
-            }
-        } else {
-            LOG("Unknown error: %s %d\n", rreq.auth.username, authenticated);
+    }
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return false;
+    }
+    if(pid == 0){
+        seteuid(req->cred.uid);
+        int status = auth_validate(req->client_fd, rreq.auth.username, NULL);
+        exit(status);
+    } else {
+        pid_t w = waitpid(pid, &authenticated, 0);
+        if (w == -1) {
+            perror("waitpid");
             return false;
         }
+        if(authenticated > 1){
+           authenticated = 1;
+        }
+    }
+    if (authenticated == AUTH_OK) {
+        LOG("Authentication success: %s\n", rreq.auth.username);
+        rresp.status = AUTH_OK;
+        write(req->client_fd, &rresp, sizeof(rresp));
+        if (req)
+            *req = rreq;
+    } else if (authenticated == AUTH_FAIL) {
+        LOG("Authentication fail: %s\n", rreq.auth.username);
+        rresp.status = AUTH_FAIL;
+        memset(&rreq.auth.password, 0, sizeof(rreq.auth.password));
+
+        write(req->client_fd, &rresp, sizeof(rresp));
+
+    } else {
+        LOG("Unknown error: %s %d\n", rreq.auth.username, authenticated);
+        return false;
     }
     return true;
 }
@@ -113,22 +109,29 @@ static int auth_validate(int client_fd, const char *username, const char *passwo
     struct pam_conv conv;
     conv.conv = sock_conv;
     conv.appdata_ptr = &client_fd;
+    size_t auth_try = 0;
 
-    LOG("Pam start\n");
-    retval = pam_start("suidless-su", username, &conv, &pamh);
-    if (retval != PAM_SUCCESS){
-        retval = pam_start("su", username, &conv, &pamh);
-    }
-    if (retval != PAM_SUCCESS){
-        LOG("Pam start fail\n");
-        return AUTH_FAIL;
-    }
+    while(auth_try < 3){
 
-    retval = pam_authenticate(pamh, 0);
-    if (retval == PAM_SUCCESS){
-        LOG("Pam acct mgmt fail\n");
-        retval = pam_acct_mgmt(pamh, 0);
+        LOG("Pam start\n");
+        retval = pam_start("suidless-su", username, &conv, &pamh);
+        if (retval != PAM_SUCCESS){
+            retval = pam_start("su", username, &conv, &pamh);
+        }
+        if (retval != PAM_SUCCESS){
+            LOG("Pam start fail\n");
+            return AUTH_FAIL;
+        }
+
+        retval = pam_authenticate(pamh, 0);
+        if (retval == PAM_SUCCESS){
+            LOG("Pam acct mgmt fail\n");
+            retval = pam_acct_mgmt(pamh, 0);
+        }
+        pam_end(pamh, retval);
+        if(retval == PAM_SUCCESS){
+            return AUTH_OK;
+        }
     }
-    pam_end(pamh, retval);
-    return retval == PAM_SUCCESS ? AUTH_OK : AUTH_FAIL;
+    return AUTH_FAIL;
 }
